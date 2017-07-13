c9dd5ae845c16a809d1279176c6d41f779915c1c
Jirffey Gabuya <boyhimas@gmail.com>
2017-07-13 19:40:56 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class TodoApp extends React.Component {
+    render() {
+        return {
+            <h2>Simple Todo App with React</h2>
+        }
+    }
+}
+
+ReactDOM.render(<TodoApp />, document.getElementById('app'));
